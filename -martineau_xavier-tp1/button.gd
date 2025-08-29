66f2d953672ef186798ab68e8c501162390ef810
XavierMartineau <143350807+XavierMartineau@users.noreply.github.com>
2025-08-29 16:54:59 -0400

    Ajout button color

--- -martineau_xavier-tp1/button.gd
+++ -martineau_xavier-tp1/button.gd
@@ -3,9 +3,11 @@ extends Button
 
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		$AudioStreamPlayer.play()
+		$AudioStreamPlayer.play()  
+		$AnimatedSprite2D.play()
+
 	else:
 		$AudioStreamPlayer.stop()
+		$AnimatedSprite2D.stop()
 	
-		
 		
--- -martineau_xavier-tp1/button_trigger.gd
+++ -martineau_xavier-tp1/button_trigger.gd
@@ -3,4 +3,4 @@ extends Button
 
 func _on_pressed() -> void:
 	$AudioStreamPlayer.play()
-	
+	$ColorRect.color = Color(randf(),randf(),randf(),randf())
